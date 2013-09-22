5a92e973e1b8ed03b3b20b4f165bd723b790d66e
vadimi <vadim.ivanou@gmail.com>
2013-09-22 12:49:16 -0400

    Use sql.Scanner for sql.NullBool, sql.NullFloat64, sql.NullInt64 and sql.NullString types

--- base.go
+++ base.go
@@ -60,24 +60,12 @@ func (d base) setModelValue(driverValue, fieldValue reflect.Value) error {
 		switch fieldValue.Interface().(type) {
 		case time.Time:
 			fieldValue.Set(driverValue.Elem())
-		case sql.NullBool:
-			b := d.dialect.parseBool(driverValue.Elem())
-			fieldValue.Set(reflect.ValueOf(sql.NullBool{b, true}))
-		case sql.NullFloat64:
-			if f, ok := driverValue.Elem().Interface().(float64); ok {
-				fieldValue.Set(reflect.ValueOf(sql.NullFloat64{f, true}))
+		default:
+			if scanner, ok := fieldValue.Addr().Interface().(sql.Scanner); ok {
+				return scanner.Scan(driverValue.Interface())
 			}
-		case sql.NullInt64:
-			if i, ok := driverValue.Elem().Interface().(int64); ok {
-				fieldValue.Set(reflect.ValueOf(sql.NullInt64{i, true}))
-			}
-		case sql.NullString:
-			str := string(driverValue.Elem().Bytes())
-			fieldValue.Set(reflect.ValueOf(sql.NullString{str, true}))
 		}
-		if scanner, ok := fieldValue.Addr().Interface().(sql.Scanner); ok {
-			return scanner.Scan(driverValue.Interface())
-		}
+
 	}
 	return nil
 }
